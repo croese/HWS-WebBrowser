574f0663818b22a5fb46905f63a3f7b1b5a1db2f
Christian Roese <croese@gmail.com>
2018-10-05 10:42:13 -0500

    Finishes Project4: Web Browser

--- Project4/ViewController.swift
+++ Project4/ViewController.swift
@@ -8,13 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+  
+  var websites = ["apple.com", "hackingwithswift.com", "google.com"]
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // Do any additional setup after loading the view.
+    
+    title = "Web Browser"
+    navigationController?.navigationBar.prefersLargeTitles = true
   }
-
-
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return websites.count
+  }
+  
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController
+    {
+      vc.websites = websites
+      vc.initialSite = websites[indexPath.row]
+      navigationController?.pushViewController(vc, animated: true)
+    }
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Site", for: indexPath)
+    
+    cell.textLabel?.text = websites[indexPath.row]
+    
+    return cell
+  }
 }
 
